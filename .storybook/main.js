8f57d51f0869303e95a375954f0704cd22782640
052hide <hidetoshi1990@gmail.com>
2022-01-23 16:28:57 +0900

    add vite config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,14 @@
+const path = require('path')
+
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
   core: {
     builder: 'storybook-builder-vite',
+  },
+  viteFinal(config, { configType }) {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src')
+    return config
   },
 }
