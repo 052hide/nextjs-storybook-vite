e2c7ba2aa2ad75aa53f26dccf88672e201f0dea1
052hide <hidetoshi1990@gmail.com>
2022-01-23 16:42:50 +0900

    productionはwebpack5でビルドする

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,7 @@
 const path = require('path')
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -16,7 +18,11 @@ module.exports = {
   ],
   framework: '@storybook/react',
   core: {
-    builder: 'storybook-builder-vite',
+    builder: isProduction ? 'webpack5' : 'storybook-builder-vite',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src')
+    return config
   },
   viteFinal(config, { configType }) {
     config.resolve.alias['@'] = path.resolve(__dirname, '../src')
